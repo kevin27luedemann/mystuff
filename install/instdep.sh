0d25755c12ea2fa9ed5eecfa90f0eddb9abe0d34
Kevin Luedemann <luedemann@localhost.localdomain>
2016-04-27 20:26:10 +0200

    instdep verbessert

--- install/instdep.sh
+++ install/instdep.sh
@@ -7,37 +7,52 @@ cp ./sources.list /etc/apt/
 apt-get update
 apt-get upgrade -y
 #erste handvoll wichtige programme
-apt-get install -y okular filezilla ncdu cowsay fortune mc feh ImageMagick arandr gcc xterm ranger hexedit i3 i3blocks pkg-config gtk+-2.0 gtk+-3.0 mpc mpd htop
+apt-get install -y okular filezilla ncdu cowsay fortune mc feh ImageMagick arandr gcc xterm ranger hexedit i3 i3blocks pkg-config gtk+-2.0 gtk+-3.0 mpc mpd htop xterm
 #vim mit latex installieren
 apt-get install -y vim-gtk vim-python-jedi vim-addon-manager vim-latexsuite vim-syntax-gtk
 #google chrome installieren
 apt-get install -y chromium
-#install nvidia driver und konfigurieren
-apt-get install -y nvidia-driver nvidia-xconfig
-apt-get install -y xorg-server-source
-nvidia-xconfig
 #install python
 apt-get install -y python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
+#install nvidia driver und konfigurieren
+if [ "$2" == "nvidia" ] || [ "$3" == "nvidia" ] || [ "$4" == "nvidia" ]
+then
+	apt-get install -y nvidia-driver nvidia-xconfig
+	apt-get install -y xorg-server-source
+	nvidia-xconfig
+	apt-get install -y nvidia-cuda-dev nvidia-cuda-toolkit nvidia-cuda-doc nvidia-cuda-mps
+fi
 #install texlive
-#apt-get install texlive-full
+if [ "$2" == "texlive" ] || [ "$3" == "texlive" ] || [ "$4" == "texlive" ]
+then
+	apt-get install texlive-full
+fi
 
 #add Resource Datai fuer xterminal
 cp ../bashinit/.Xresources /home/$1/
+chmod 646 /home/$1/.Xresources
 #i3 einstellen
 cp ../i3/wallpaper.jpg /home/$1/Bilder/
+chmod 646 /home/$1/Bilder/wallpaper.jpg
 mkdir -p /home/$1/.i3/
 cp ../i3/config /home/$1/.i3/config
+chmod 646 /home/$1/.i3/config
 #bash einstelln
 cp ../bashinit/.bashrc /home/$1/ ;
+chmod 646 /home/$1/.bashrc
 cp ../bashinit/.bash_aliases /home/$1/ ;
+chmod 646 /home/$1/.bash_aliases
 cp ../bashinit/.bash_profile /home/$1/ ;
+chmod 646 /home/$1/.bash_profile
 cp ../scripte/.falsch.sh /home/$1/ ;
+chmod 646 /home/$1/.falsch.sh
 cp ../bashinit/.vimrc /home/$1/ ;
+chmod 646 /home/$1/.vimrc
 
 #DVD codec css installieren
 apt-get install libdvdread4 ;
 mkdir ./dvd/ ;
-cp libdvdcss-1.2.13.tar.bz2 dvd/ ;
+mv libdvdcss-1.2.13.tar.bz2 dvd/ ;
 cd dvd/ ;
 tar -xf libdvdcss-1.2.13.tar.bz2 ;
 cd libdvdcss-1.2.13 ;
@@ -49,11 +64,11 @@ rm -rf dvd/ ;
 #diable beeping
 rmmod pcspkr
 
-if [ "$2" == "real" ]
+if [ "$2" == "real" ] || [ "$3" == "real" ] || [ "$4" == "real" ]
 then
 apt-get install -y firmware-realtek
 fi
 
 else
-echo "USER ist nicht gesetzt"
+	echo "USER ist nicht gesetzt"
 fi
