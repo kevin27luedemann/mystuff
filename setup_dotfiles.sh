9999ff0190a2a0961ab119083774c560bd91d0d2
Kevin Luedemann <kevin27luedemann@googlemail.com>
2021-09-11 14:23:58 +0200

    Setup file for dotfiles

---
+++ setup_dotfiles.sh
@@ -0,0 +1,5 @@
+ln -s /home/$HOME/Documents/mystuff/dotfiles/.i3/ /home/$HOME/
+ln -s /home/$HOME/Documents/mystuff/dotfiles/.i3status.conf /home/$HOME/
+ln -s /home/$HOME/Documents/mystuff/dotfiles/.bashrc /home/$HOME/
+ln -s /home/$HOME/Documents/mystuff/dotfiles/.bash_aliases /home/$HOME/
+ln -s /home/$HOME/Documents/mystuff/dotfiles/.bash_profile /home/$HOME/
