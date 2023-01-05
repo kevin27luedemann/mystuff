a7f25393b6d273e03566d0bb2618c6080b5507fa
Kevin Luedemann <kevin27luedemann@googlemail.com>
2023-01-05 08:14:55 +0100

    Homogeneous lock screen script
    The lock script starts i3lock with my settings in any case.
    Passing "b" to it will blank the screen after 5 seconds and deactivate
    the display output. Passing "h" will further put the device into suspend
    to RAM if the mode exists.

--- dotfiles/.i3/lock.sh
+++ dotfiles/.i3/lock.sh
@@ -2,7 +2,17 @@
 revert(){
     xset dpms 0 0 0
 }
-trap revert HUP INT TERM
-xset +dpms dpms 5 5 5
+#Activate screen blanking for energy saving
+if [ "$1" = "b" ] || [ "$1" = "h" ]
+then
+    trap revert HUP INT TERM
+    xset +dpms dpms 5 5 5
+fi
+
+#Suspend the device to RAM to further save energy
+if [ "$1" = "h" ]
+then
+    systemctl suspend
+fi
 i3lock -n -t -i $HOME/.wallpaper_logo_saver.png -k --timecolor=D50000 --datecolor=000080 --timepos="100:50" --datestr="%A %d.%m.%Y"
 revert
