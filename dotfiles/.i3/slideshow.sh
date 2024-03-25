d5cb7e2e7414cf27fe6f28b6a607bb61c933dc57
Kevin Luedemann <kevin27luedemann@googlemail.com>
2024-03-25 09:37:29 +0100

    Update slideshow

--- dotfiles/.i3/slideshow.sh
+++ dotfiles/.i3/slideshow.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-
-FOLDERS=`ls $HOME/Nextcloud/pictures/ | grep -v "20230707" | grep -v "20230720"`
-echo $FOLDERS
-
-FOLD=
-for fil in $FOLDERS
-do
-	FOLD="$FOLD $HOME/Nextcloud/pictures/$fil/"
-done
-
-feh -Y -x -q -D 10 -B black -F -Z -z -r $FOLD
-
+feh -Y -x -q -D 10 -B black -F -Z -z -f $FOLD
