968fb43d4f1d7c66c2bf1769bca57cc06e6ebf4a
Kevin Luedemann <kevin27luedemann@googlemail.com>
2024-03-25 09:15:46 +0100

    Add slideshow script

---
+++ dotfiles/.i3/slideshow.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FOLDERS=`ls $HOME/Nextcloud/pictures/ | grep -v -E "(20230707|20230720)"`
+echo $FOLDERS
+
+FOLD=
+for fil in $FOLDERS
+do
+	FOLD="$FOLD $HOME/Nextcloud/pictures/$fil/"
+done
+
+feh -Y -x -q -D 10 -B black -F -Z -z -r $FOLD
+
