0ff80a5a6a834e4b8e5dbcaa2891e1516b3e3cf0
Kevin Luedemann <kevin27luedemann@googlemail.com>
2025-02-23 13:09:07 +0100

    Update airbrain backlight

--- dotfiles/.i3/backlight.sh
+++ dotfiles/.i3/backlight.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ "$HOSTNAME" = "airbrain" ]; then
 	if [ "$1" = "dec" ]; then
-		xbacklight -dec $2
+		light -U $2
 	elif [ "$1" = "inc" ]; then
-		xbacklight -inc $2
+		light -A $2
 	fi
 elif [ "$HOSTNAME" = "framework" ]; then
 	if [ "$1" = "dec" ]; then
