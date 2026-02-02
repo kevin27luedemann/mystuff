f754cff34e989465e6291d31194d6619fd5b9342
Kevin Luedemann <kevin.luedemann@gwdg.de>
2026-02-02 15:14:19 +0100

    Update backlight for work laptop

--- dotfiles/.i3/backlight.sh
+++ dotfiles/.i3/backlight.sh
@@ -11,7 +11,7 @@ elif [ "$HOSTNAME" = "framework" ]; then
 	elif [ "$1" = "inc" ]; then
 		light -A $2
 	fi
-elif [ "$HOSTNAME" = "kevin-Latitude-7420" ]; then
+elif [ "$HOSTNAME" = "kevin-latitude7420" ]; then
 	if [ "$1" = "dec" ]; then
 		light -U $2
 	elif [ "$1" = "inc" ]; then
