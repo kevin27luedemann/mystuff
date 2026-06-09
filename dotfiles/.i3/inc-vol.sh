14cce995baa13f31e52bc6d12a98c14655249903
Kevin Luedemann <kevin27luedemann@googlemail.com>
2026-06-09 11:25:12 +0200

    Update airbrain volume settings

--- dotfiles/.i3/inc-vol.sh
+++ dotfiles/.i3/inc-vol.sh
@@ -3,6 +3,8 @@ if [ "$HOSTNAME" = "framework" ]; then
 	amixer -c 0 set Master 2%+
 elif [ "$HOSTNAME" = "AGC-kevin" ]; then
 	amixer set Master 2%+
+elif [ "$HOSTNAME" = "airbrain" ]; then
+	amixer set Master 2%+
 else
 	amixer -c 1 set Master 2%+
 fi
